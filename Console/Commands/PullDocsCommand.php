cb9f5c8d9bbbe1336d6f3955983283243d04dbbf
Dan Aldridge <xLink@cybershade.org>
2016-12-07 17:49:24 +0000

    added module:pull-docs

---
+++ Console/Commands/PullDocsCommand.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace Cms\Modules\Docs\Console\Commands;
+
+use Illuminate\Console\Command;
+use Pingpong\Modules\Process\Installer;
+use Pingpong\Modules\Repository;
+
+class PullDocsCommand extends Command
+{
+    protected $name = 'module:pull-docs';
+    protected $readableName = 'Pulls the CMSs Documentation';
+    protected $description = 'Pulls the CMSs Documentation';
+
+    public function fire()
+    {
+        $files = app('Illuminate\Filesystem\Filesystem');
+
+        $docPath = storage_path('docs');
+
+        // make sure the directory is empty
+        if (!$files->isDirectory($docPath)) {
+            $files->makeDirectory($docPath);
+        } else {
+            $files->deleteDirectory($docPath);
+            $files->makeDirectory($docPath);
+        }
+
+        // spawn the repository
+        ob_start();
+        $installer = new Installer(
+            'Cysha/Documentation',
+            null,
+            'github',
+            false
+        );
+
+        $installer->setPath($docPath.'/master');
+
+        $repository = new Repository($this->laravel, $docPath);
+        $installer->setRepository($repository);
+
+        $installer->setConsole($this);
+
+        $installer->run();
+        ob_end_clean();
+    }
+
+    protected function getOptions()
+    {
+        return [
+        ];
+    }
+}
--- Providers/DocsModuleServiceProvider.php
+++ Providers/DocsModuleServiceProvider.php
@@ -3,6 +3,7 @@
 namespace Cms\Modules\Docs\Providers;
 
 use Cms\Modules\Core\Providers\BaseModuleProvider;
+use Cms\Modules\Docs\Console\Commands\PullDocsCommand;
 
 class DocsModuleServiceProvider extends BaseModuleProvider
 {
@@ -23,6 +24,7 @@ class DocsModuleServiceProvider extends BaseModuleProvider
      */
     protected $commands = [
         'Docs' => [
+            'PullDocsCommand',
         ],
     ];
 }
